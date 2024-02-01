fe6749438f803f3263e544a6e3d673e83697e50a
aliirz <dejavu87@gmail.com>
2024-02-01 14:58:37 +0500

    about is updated

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -2,8 +2,18 @@
     <div class="grid grid-cols-3 gap-4 mx-auto max-w-mw w-full h-full p-8">
         <h1 class="text-4xl font-bold mb-4 col-span-3 text-red-900">About Intikhabi Mela</h1>
         <img class="w-full h-64 object-cover mb-8 col-span-3" src="/heroimg.png" alt="About image">
-        <p class="text-lg mb-4 col-span-3 text-red-900">Join us to explore the Art & Science behind the upcoming general elections! Join a community of artists eager to express their views on democracy. Bring your art supplies, share your ideas, and connect with fellow artists, engineers, and passionate citizens.</p>
-
+        <p class="text-lg mb-4 col-span-3 text-red-900">Welcome to our special webpage dedicated to the "Intikhabi Mela," a unique event organized by the Code for Pakistan civic community. Join us in celebrating the fusion of art, technology, and civic participation, all aimed at empowering democracy through our collective voices and skills.
+        </p>
+        <p class="text-lg mb-4 col-span-3 text-red-900">
+            We at Code for Pakistan believe that art and technology are not just tools but powerful catalysts for civic engagement. The Intikhabi Mela envisions a creative approach to addressing voter rights, election awareness, and fostering community involvement.
+        </p>
+        <p class="text-lg mb-4 col-span-3 text-red-900">
+            This page features an inspiring gallery of artworks and projects created during the Intikhabi Mela. Each piece embodies the spirit of informed voting and the power of creative expression in nurturing a robust democracy. Browse through our collection and witness how art and technology can come together to make a meaningful difference in our society.
+        </p>
+        <p class="text-lg mb-4 col-span-3 text-red-900">
+            Join our community of volunteers – where your voice, creativity, and technology unite to shape the future of democracy.
+            Email us <a class="text-blue-900" href="mailto:info@codeforpakistan.org">info@codeforpakistan.org</a> or follow us on our socials to stay updated!
+        </p>
     </div>
 </template>
 
@@ -13,3 +23,4 @@ export default {
     path: '/about'
 }
 </script>
+
